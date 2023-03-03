da2ed573518965e7043133e7dfc040c047635c11
syb8200 <syb8200@naver.com>
2023-03-03 13:20:49 +0900

    first commit

---
+++ app/src/main/java/fastcampus/part1/fc_chapter3/MainActivity.kt
@@ -0,0 +1,13 @@
+package fastcampus.part1.fc_chapter3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fastcampus.part1.fc_chapter3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
