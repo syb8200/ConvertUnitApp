75da6106348aca36c83671d88ba459870c085bd6
syb8200 <syb8200@naver.com>
2023-03-03 14:12:26 +0900

    단위 변환 기능 구현 (cm → m)

--- app/src/main/java/fastcampus/part1/fc_chapter3/MainActivity.kt
+++ app/src/main/java/fastcampus/part1/fc_chapter3/MainActivity.kt
@@ -2,6 +2,7 @@ package fastcampus.part1.fc_chapter3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
 import fastcampus.part1.fc_chapter3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +15,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view  = binding.root
         setContentView(view)
+
+        val outputTextView = binding.outputTextView
+        val outputUnitTextView = binding.outputUnitTextView
+        val inputEditText = binding.inputEditText
+        val inputUnitTextView = binding.inputUnitTextView
+
+        var inputNumber : Int = 0
+
+        inputEditText.addTextChangedListener { text ->
+            inputNumber = if(text.isNullOrEmpty()) {
+                0
+            } else {
+                text.toString().toInt()
+            }
+
+            var outputNumber = inputNumber.times(0.01)
+            outputTextView.text = outputNumber.toString()
+        }
     }
 }
