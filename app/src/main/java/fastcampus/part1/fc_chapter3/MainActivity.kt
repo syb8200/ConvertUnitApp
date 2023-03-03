2676ca71dd6e585702b001362ad8399afbf24c4d
syb8200 <syb8200@naver.com>
2023-03-03 15:28:02 +0900

    화면 전환 시 데이터 유지 기능 구현

--- app/src/main/java/fastcampus/part1/fc_chapter3/MainActivity.kt
+++ app/src/main/java/fastcampus/part1/fc_chapter3/MainActivity.kt
@@ -9,6 +9,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    var inputNumber : Int = 0
+    var cmToM = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,9 +24,6 @@ class MainActivity : AppCompatActivity() {
         val inputEditText = binding.inputEditText
         val inputUnitTextView = binding.inputUnitTextView
         val swapImageButton = binding.swapImageButton
-
-        var inputNumber : Int = 0
-        var cmToM = true
 
         inputEditText.addTextChangedListener { text ->
             inputNumber = if(text.isNullOrEmpty()) {
@@ -49,8 +49,20 @@ class MainActivity : AppCompatActivity() {
             } else {
                 inputUnitTextView.text = "m"
                 outputUnitTextView.text = "cm"
-                outputTextView.text = inputNumber.times(100).toString()
+                 outputTextView.text = inputNumber.times(100).toString()
             }
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean("cmToM", cmToM)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        cmToM = savedInstanceState.getBoolean("cmToM")
+        binding.inputUnitTextView.text = if(cmToM) "cm" else "m"
+        binding.outputUnitTextView.text = if(cmToM) "m" else "cm"
+        super.onRestoreInstanceState(savedInstanceState)
     }
 }
